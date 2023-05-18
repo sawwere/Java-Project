ffa6d8284f19f1b67ad247e7a1b3a515ce3ee012
sawwere <sawer_13@mail.ru>
2023-05-18 17:57:19 +0300

    Initial commit

---
+++ src/main/java/com/javaproject/allUp/AllUpApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.allUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/allUp/AllUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.allUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
